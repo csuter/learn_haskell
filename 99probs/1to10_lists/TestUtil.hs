c979b3718655523916a07c6c5ac1635e7b1c61b5
Christopher Suter <cgs1019@gmail.com>
2017-11-25 02:34:22 -0800

    Cleanup old files

--- 99probs/1to10_lists/TestUtil.hs
+++
@@ -1,9 +0,0 @@
-module TestUtil
-( expectEq
-) where
-
-expectEq :: (Eq a, Show a) => a -> a -> Bool
-expectEq x y
-    | x == y = True
-    | x /= y = error (
-        "Expected equal values, got " ++ (show x) ++ " and " ++ (show y))
